63d452a04a87476832506ec08f5ee72cd02d1e8b
박주호 <juho7410@gmail.com>
2023-11-14 16:05:59 +0900

    상태 호이스팅 & 지연 목록 생성

--- app/src/main/java/com/pachuho/composestudy/MainActivity.kt
+++ app/src/main/java/com/pachuho/composestudy/MainActivity.kt
@@ -3,21 +3,26 @@ package com.pachuho.composestudy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.pachuho.composestudy.ui.theme.ComposeStudyTheme
@@ -34,12 +39,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-private fun MyApp(
-    modifier: Modifier,
-    names: List<String> = listOf("World", "Compose")
+fun OnboardScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier
 ){
-    Column(modifier) {
-        for (name in names){
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Welcom to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text(text = "Continue")
+        }
+    }
+}
+
+@Composable
+private fun MyApp(modifier: Modifier){
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+
+    Surface(modifier) {
+        if(shouldShowOnboarding){
+            OnboardScreen(onContinueClicked = { shouldShowOnboarding = false })
+        } else {
+            Greetings()
+        }
+    }
+}
+
+@Composable
+fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = List(1000) { "$it" }
+){
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
             Greeting(name = name)
         }
     }
@@ -78,10 +116,26 @@ fun Greeting(name: String) {
     }
 }
 
+@Preview
+@Composable
+fun MyAppPreview() {
+    ComposeStudyTheme {
+        MyApp(Modifier.fillMaxSize())
+    }
+}
+
 @Preview(showBackground = true, widthDp = 320, heightDp = 640)
 @Composable
-fun GreetingPreview() {
+fun GreetingsPreview() {
     ComposeStudyTheme {
-        MyApp(modifier = Modifier.fillMaxSize())
+        Greetings()
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnBoardingPreview(){
+    ComposeStudyTheme {
+        OnboardScreen(onContinueClicked = {})
     }
 }
